789d53ca4190a80de246041f6be6403880550957
张苏杭 <zhangsuhang04551@gmail.com>
2024-05-10 19:40:08 +0800

    2024年5月10日19:40:02

---
+++ UIMasterProject/Assets/UI-Master/5.Scripts/AchievementSystem/AchievenmentListIngame.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Add list of achievements to screen
+/// </summary>
+public class AchievenmentListIngame : MonoBehaviour
+{
+    public GameObject scrollContent;
+    public GameObject prefab;
+
+    public Text CountText;
+    public Text CompleteText;
+    public Scrollbar Scrollbar;
+
+
+    private void OnEnable()
+    {
+        AddAchievements();
+    }
+
+    /// <summary>
+    /// Adds all achievements to the UI based on a filter
+    /// </summary>
+    /// <param name="Filter">Filter to use (All, Achieved or Unachieved)</param>
+    private void AddAchievements()
+    {  
+        foreach (Transform child in scrollContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        UIM_AchievenManager AM = UIM_AchievenManager.instance;
+
+       // CountText.text = "" + AM.AchievementList.Count + " / " + AM.AchievementList.Count;
+        //CompleteText.text = "Complete (" + AM.GetAchievedPercentage() + "%)";
+
+        for (int i = 0; i < AM.AchievementList.Count; i ++)
+        {
+            var state = AM.AchievementList[i].State;
+            AddAchievementToUI(AM.AchievementList[i]);
+        }
+        //Scrollbar.value = 1;
+    }
+
+    public void AddAchievementToUI(AchievementInfromation Achievement)
+    {
+        UIAchievement UIAchievement = Instantiate(prefab).GetComponent<UIAchievement>();
+        UIAchievement.SetDisplay(Achievement);
+        UIAchievement.transform.SetParent(scrollContent.transform);
+        UIAchievement.transform.localScale = Vector3.one;
+    }
+
+
+
+
+}
